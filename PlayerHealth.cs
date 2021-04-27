cc26af95dde5ccfd05edcc0fe902211c7ea29698
Chief-Beef <60185920+Chief-Beef@users.noreply.github.com>
2021-04-27 13:43:30 -0400

    Add files via upload

---
+++ PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
